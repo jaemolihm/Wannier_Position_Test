5d645376506ec8fbfb5fb1004524b137ef254ff2
Jae-Mo Lihm <jaemolim96@gmail.com>
2022-05-25 04:25:48 +0900

    Add si

---
+++ si_unitcell_shift1.0/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex
+
+mpirun -np 4 pw.x -in scf.in > scf.out
+mpirun -np 4 pw.x -in nscf.in > nscf.out
+
+wannier90.x -pp si
+mpirun -np 4 pw2wannier90.x -in pw2wan.in > pw2wan.out
+wannier90.x si
